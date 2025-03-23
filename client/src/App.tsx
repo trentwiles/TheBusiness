9ba6afc14a9368b4e9e836341b1c542cb22d4882
trentwiles <wilest7@gmail.com>
2025-03-23 11:40:07 -0400

    tweak shadcn login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import Sidebar from './components/customs/Sidebar'
-import OrderGrid from './components/customs/OrderGrid'
+// import OrderGrid from './components/customs/OrderGrid'
+import Login from './components/customs/Login'
 
 const App = () => {
   return (
-    <Sidebar children={<OrderGrid apiPath='http://localhost:5000/orders' />} />
+    // <Sidebar children={<OrderGrid apiPath='http://localhost:5000/orders' />} />
+    <Sidebar children={<Login enableCreateAccount={false} enableOauth={false} />} />
   )
 }
 
