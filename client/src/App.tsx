473ae775aecbeec15d17a85f005119c7064c536d
trentwiles <wilest7@gmail.com>
2025-03-25 23:07:55 -0400

    enough for tn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,33 @@
-import Sidebar from './components/customs/Sidebar'
+import Sidebar from "./components/customs/Sidebar";
 // import OrderGrid from './components/customs/OrderGrid'
 // import Login from './components/customs/Login'
-import Tracking from './components/customs/Tracking'
-import {Status} from './components/customs/Tracking'
+import Tracking from "./components/customs/Tracking";
+import { Status } from "./components/customs/Tracking";
+import { SingleOrder, OrderItem } from "./components/customs/OrderGrid";
 
 const App = () => {
+  const orderItemOne: OrderItem = { Lemonade: 2.39 };
+  const orderItemTwo: OrderItem = { "Coke Zero": 12.45 };
+
+  const exampleOrder: SingleOrder = {
+    client: "James McDavid",
+    id: "981290382838923932",
+    orderTitle: "Some Random Order",
+    orderItems: [orderItemTwo, orderItemOne],
+  };
   return (
     // <Sidebar children={<OrderGrid apiPath=`${import.meta.env.BASE_URL}/orders` />} />
     // <Sidebar children={<Login enableCreateAccount={false} enableOauth={false} />} />
-    <Sidebar children={<Tracking currentStatus={Status.Preparing}/>} />
-  )
-}
+    <Sidebar
+      children={
+        <Tracking
+          currentStatus={Status.Preparing}
+          orderID={Math.floor(Math.random() * Math.pow(10, 16))}
+          orders={exampleOrder.orderItems}
+        />
+      }
+    />
+  );
+};
 
-export default App
+export default App;
--- client/src/components/customs/Tracking.tsx
+++ client/src/components/customs/Tracking.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import {useState} from "react";
+
 import { Progress } from "@/components/ui/progress";
+
+import { OrderItem } from "@/components/customs/OrderGrid";
 
 import {
   Card,
@@ -12,12 +15,27 @@ import {
 
 import { Button } from "@/components/ui/button";
 
+import { ChevronsUpDown } from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+
 export enum Status {
   Preparing,
   Collecting,
   Awaiting,
   Completed,
 }
+
+const statusToProgressBarSize: { [key in Status]: number } = {
+  [Status.Preparing]: 25,
+  [Status.Collecting]: 50,
+  [Status.Awaiting]: 75,
+  [Status.Completed]: 100,
+};
 
 type props = {
   customTitle?: string | "Order Status";
@@ -32,19 +50,33 @@ type props = {
   customCompletedTitle?: string;
   customCompletedDesc?: string;
 
+  // REQUIRED
+  orderID: number;
   currentStatus: Status;
+  orders: OrderItem[];
 };
 
 export default function Tracking(props: props) {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         {props.customTitle || "Preparing Order"}
       </h1>
-      <Progress value={25} />
+      <p className="text-sm text-muted-foreground mb-10">
+        Order ID: {props.orderID}
+      </p>
+      <Progress value={statusToProgressBarSize[props.currentStatus]} />
       {/* four steps: assign to worker, gathering, delivery, completed */}
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 gap-4">
-      <Card className={`m-4 ${(props.currentStatus >= Status.Preparing) ? '' : 'opacity-50 pointer-events-none'}`}>
+        <Card
+          className={`m-4 ${
+            props.currentStatus >= Status.Preparing
+              ? ""
+              : "opacity-50 pointer-events-none"
+          }`}
+        >
           <CardHeader>
             <CardTitle>{props.customPreparingTitle || "Preparing"}</CardTitle>
             <CardDescription>
@@ -54,7 +86,13 @@ export default function Tracking(props: props) {
           </CardHeader>
           <CardContent>{/* messages/details here */}</CardContent>
         </Card>
-        <Card className={`m-4 ${(props.currentStatus >= Status.Collecting) ? '' : 'opacity-50 pointer-events-none'}`}>
+        <Card
+          className={`m-4 ${
+            props.currentStatus >= Status.Collecting
+              ? ""
+              : "opacity-50 pointer-events-none"
+          }`}
+        >
           <CardHeader>
             <CardTitle>{props.customCollectingTitle || "Collecting"}</CardTitle>
             <CardDescription>
@@ -64,7 +102,13 @@ export default function Tracking(props: props) {
           </CardHeader>
           <CardContent>{/* messages/details here */}</CardContent>
         </Card>
-        <Card className={`m-4 ${(props.currentStatus >= Status.Awaiting) ? '' : 'opacity-50 pointer-events-none'}`}>
+        <Card
+          className={`m-4 ${
+            props.currentStatus >= Status.Awaiting
+              ? ""
+              : "opacity-50 pointer-events-none"
+          }`}
+        >
           <CardHeader>
             <CardTitle>
               {props.customAwaitingExchangeTitle || "Awaiting Pickup"}
@@ -76,7 +120,13 @@ export default function Tracking(props: props) {
           </CardHeader>
           <CardContent>{/* messages/details here */}</CardContent>
         </Card>
-        <Card className={`m-4 ${(props.currentStatus >= Status.Completed) ? '' : 'opacity-50 pointer-events-none'}`}>
+        <Card
+          className={`m-4 ${
+            props.currentStatus >= Status.Completed
+              ? ""
+              : "opacity-50 pointer-events-none"
+          }`}
+        >
           <CardHeader>
             <CardTitle>{props.customCollectingTitle || "Completed"}</CardTitle>
             <CardDescription>
@@ -87,6 +137,39 @@ export default function Tracking(props: props) {
           <CardContent>{/* messages/details here */}</CardContent>
         </Card>
       </div>
+      {/* your order section */}
+      <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl px-2">
+        Order Details
+      </h1>
+      <ul>
+        
+      </ul>
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-[350px] space-y-2"
+      >
+        <div className="flex items-center justify-between space-x-4 px-4">
+          <h4 className="text-sm font-semibold">
+            {props.orders.length} items
+          </h4>
+          <CollapsibleTrigger asChild>
+            <Button variant="ghost" size="sm">
+              <ChevronsUpDown className="h-4 w-4" />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </CollapsibleTrigger>
+        </div>
+        <CollapsibleContent className="space-y-2">
+        {props.orders.map((order, index) =>
+          Object.entries(order).map(([itemName, itemPrice]) => (
+            <div key={index} className="rounded-md border px-4 py-2 text-sm shadow-sm">
+              {itemName} (${itemPrice.toFixed(2)})
+            </div>
+          ))
+        )}
+        </CollapsibleContent>
+      </Collapsible>
     </>
   );
 }
