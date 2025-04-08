1487bdb257a29f2b3c4d6e6ae37be1ab9bb15bd9
trentwiles <wilest7@gmail.com>
2025-04-08 15:00:23 -0400

    clean App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,100 +17,18 @@ import { AuthProvider } from "./components/auth/AuthProvider";
 import DasherQueue from "./components/customs/DasherQueue";
 
 import RouteWrapper from "@/components/routing/RouterWrapper";
+import SalesChart from "./components/customs/MonthSalesChart";
 
 const App = () => {
-  // const orderItemOne: OrderItem = { Lemonade: 2.39 };
-  // const orderItemTwo: OrderItem = { "Coke Zero": 12.45 };
-
-  // const exampleOrder: SingleOrder = {
-  //   client: "James McDavid",
-  //   id: "981290382838923932",
-  //   orderTitle: "Some Random Order",
-  //   orderItems: [orderItemTwo, orderItemOne],
-  // };
   const [subclass, setSubclass] = useState("");
   const [subclassTo, setSubclassTo] = useState("");
   const [pageTitle, setPageTitle] = useState("");
 
-  /* PAGE DEFINITIONS */
-
-  function LoginPage({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("Login");
-      setSubclass();
-    }, []);
-
-    return <Login enableCreateAccount={false} enableOauth={false} />;
-  }
-
-  function OrdersPage({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("Orders");
-      setSubclass("Administrator");
-    }, []);
-
-    return <OrderGrid />;
-  }
-
-  function MySales({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("Sales Charts");
-      setSubclass("Dasher");
-    }, []);
-
-    return <SlidingChart />;
-  }
-
-  function TrackingPage({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("Order Tracking");
-      setSubclass("Customer");
-    }, []);
-
-    return <Tracking />;
-  }
-
-  function HomePage({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("");
-      setSubclass("");
-    }, []);
-
-    return <Home />;
-  }
-
-  function NotFound({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("");
-      setSubclass("");
-    }, []);
-    return <FourOhFour />;
-  }
-
-  function DasherOrderQueue({ setPageTitle, setSubclass }) {
-    useEffect(() => {
-      setPageTitle("Order Queue");
-      setSubclass("Dasher");
-    }, []);
-    return <DasherQueue />;
-  }
-
-  /* END PAGE DEFINITONS */
+  useEffect(() => {
+    document.title = `${pageTitle} | The Business`;
+  }, [pageTitle]);
 
   return (
-    // <Sidebar children={<OrderGrid apiPath=`${import.meta.env.BASE_URL}/orders` />} />
-    // <Sidebar children={<Login enableCreateAccount={false} enableOauth={false} />} />
-    // <Sidebar
-    //   children={
-    //     <Tracking
-    //       currentStatus={Status.Preparing}
-    //       orderID={Math.floor(Math.random() * Math.pow(10, 16))}
-    //       orders={exampleOrder.orderItems}
-    //     />
-    //   }
-    // />
-
-    //  <Sidebar dataMode="Administrator" pageSubclass="Orders" pageTitle="More Information" children={<SlidingChart />}/>
     <AuthProvider>
       <Router>
         <Sidebar
@@ -124,45 +42,76 @@ const App = () => {
             <Route
               path="/"
               element={
-                <HomePage
-                  setSubclass={setSubclass}
+                <RouteWrapper
+                  element={Home}
+                  title="Home"
+                  subclass=""
+                  subclassLink="/"
                   setPageTitle={setPageTitle}
+                  setSubclass={setSubclass}
+                  setSubclassLink={setSubclassTo}
                 />
               }
             />
             <Route
               path="/orders"
               element={
-                <OrdersPage
-                  setSubclass={setSubclass}
+                <RouteWrapper
+                  element={OrderGrid}
+                  title="My Orders"
+                  subclass="Customer"
+                  subclassLink="/"
                   setPageTitle={setPageTitle}
+                  setSubclass={setSubclass}
+                  setSubclassLink={setSubclassTo}
                 />
               }
             />
             <Route
               path="/dasher/sales"
               element={
-                <MySales
-                  setSubclass={setSubclass}
+                <RouteWrapper
+                  element={() => <SalesChart month="1" />}
+                  title="Monthly Sales"
+                  subclass="Dasher"
+                  subclassLink="/dasher"
                   setPageTitle={setPageTitle}
+                  setSubclass={setSubclass}
+                  setSubclassLink={setSubclassTo}
                 />
               }
             />
             <Route
               path="/login"
               element={
-                <LoginPage
-                  setSubclass={setSubclass}
+                // <LoginPage
+                //   setSubclass={setSubclass}
+                //   setPageTitle={setPageTitle}
+                // />
+                <RouteWrapper
+                  element={() => (
+                    <Login enableCreateAccount={false} enableOauth={false} />
+                  )}
+                  title="Log In"
+                  subclass=""
+                  subclassLink="/"
                   setPageTitle={setPageTitle}
+                  setSubclass={setSubclass}
+                  setSubclassLink={setSubclassTo}
                 />
               }
             />
             <Route
               path="/track/:id"
               element={
-                <TrackingPage
-                  setSubclass={setSubclass}
+                <RouteWrapper
+                  element={Tracking}
+                  title="Order Tracker"
+                  subclass="Customer"
+                  subclassLink="/orders"
                   setPageTitle={setPageTitle}
+                  setSubclass={setSubclass}
+                  setSubclassLink={setSubclassTo}
                 />
               }
             />
